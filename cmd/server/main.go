0f5a6edf964f6feb5e5ff7f733a74fa938c34b5f
MuhammadMuhidin <85509638+MuhammadMuhidin@users.noreply.github.com>
2026-02-10 12:55:39 +0000

    spbase uri

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,9 +10,9 @@ import (
 )
 
 func main() {
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := os.Getenv("SUPABASE_DB_URL")
 	if dsn == "" {
-		log.Fatal("DATABASE_URL is required")
+		log.Fatal("SUPABASE_DB_URL is required")
 	}
 
 	database, err := db.New(dsn)
